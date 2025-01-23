219f1c3a07b306dab47f6cf141c4c879513b41f2
Juan Frauca <juanfrauca@outlook.com>
2025-01-23 10:34:28 +0100

    Configuracion de persistencia

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ Orden.hasMany(Pausa, { as: 'pausas' });
 Pausa.belongsTo(Orden);
 
 // Sincronizar modelos con la base de datos
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ alter: true }).then(() => {
     console.log('Base de datos sincronizada.');
 }).catch(err => console.error('Error al sincronizar la base de datos:', err));
 
